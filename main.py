de38ab96817ead718baa1e0305550574f399919d
Gunshot gaming <94213144+gunshotop@users.noreply.github.com>
2021-12-22 05:14:44 +0000

    i changed some syllabus commands.

--- main.py
+++ main.py
@@ -16,8 +16,12 @@ async def on_message(message):
     await message.channel.send('hello!')
   elif message.content.startswith('hi'):
     await message.channel.send('whatsup!')  
-  elif message.content.startswith('$syllabus'):
-    await message.channel.send("syllabus hasn't been uploaded yet! stay tune!")
+  if message.content.startswith('$syllabus sst'):
+    await message.channel.send("GEOGRAPHY:- \n 1. Resources:Utilisation and Development \n 2.Natural resources:Land,Soil,Water \n 3.Natural resorces:Vegetation and Wildlife \n 4.Agriculture \n 5. Human Resources \n HISTORY:- \n 1.Modern Period \n 2.Establishment of Company Rule in India \n 3.Colonialism:Rural and Tribal socities \n 4. The First war of Independence -1857 \n 5.The nationalist movement(1870 to 1947)\n 6.India marches ahead \n CIVICS:- \n 1.Our Constitution \n 2.Fundamental rights ,Fundamental Duties and Directive Principles of State Policy \n 3.The Union Government: The legislature \n 4.The Union Government: The Executive \n 5.The Union Government:The Judiciary")
+  if message.content.startswith('$syllabus science'):
+    await message.channel.send("syllabus has'nt been  uploaded yet")
+  if message.content.startswith('$syllabus'):
+    await message.channel.send("to get syllabus of any subject type '$syllabus <subject name>'")   
   elif  message.content.startswith('$examdate'):
     await message.channel.send('the upcoming exam is going to be a preparetory! \n exam date: 17th january 2022!')
   elif  message.content.startswith('commands'):
