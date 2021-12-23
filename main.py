9fd69375fef82ad4d118a7d4db9856ce7a14bcf0
Gunshot gaming <94213144+gunshotop@users.noreply.github.com>
2021-12-23 04:31:42 +0000

    Umai command and improved the bot coding

--- main.py
+++ main.py
@@ -53,9 +53,10 @@ async def on_message(message):
   if message.content.lower().startswith('hello'):
     await message.channel.send('hello!')
 
-  elif message.content.lower().startswith('hi'):
-    await message.channel.send('whatsup!') 
-
+  if message.content.lower().startswith('hi'):
+    await message.channel.send('whatsup!')
+  if message.content.lower().startswith("umai"):
+    await message.channel.send('Umai!')
   if message.content.lower().startswith('$syllabus sst'):
     await message.channel.send("GEOGRAPHY:- \n 1. Resources:Utilisation and Development \n 2.Natural resources:Land,Soil,Water \n 3.Natural resorces:Vegetation and Wildlife \n 4.Agriculture \n 5. Human Resources \n HISTORY:- \n 1.Modern Period \n 2.Establishment of Company Rule in India \n 3.Colonialism:Rural and Tribal socities \n 4. The First war of Independence -1857 \n 5.The nationalist movement(1870 to 1947)\n 6.India marches ahead \n CIVICS:- \n 1.Our Constitution \n 2.Fundamental rights ,Fundamental Duties and Directive Principles of State Policy \n 3.The Union Government: The legislature \n 4.The Union Government: The Executive \n 5.The Union Government:The Judiciary")
 
